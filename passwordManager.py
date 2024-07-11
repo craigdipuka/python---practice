9fedf06f7d68211be493b8f45e28027397530eb1
Craig Dipuka <craiigdipuka@gmail.com>
2024-07-11 20:58:04 +0200

    first commit

---
+++ passwordManager.py
@@ -0,0 +1,9 @@
+pwd  =input("please enter a master password : ")
+
+mode = input("would you like to enter a new password or view old password (view/add) ?\n")
+if mode =="view":
+    pass
+elif mode =="add":
+    pass
+else:
+    print("invalid mode suggested")
